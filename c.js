759f577edbe2d85586e133aa844108a02e51926e
Jahnvi-23-Pathak <jp230601@gmail.com>
2021-09-21 18:30:34 +0530

    This is my first web project

---
+++ c.js
@@ -0,0 +1,15 @@
+function display() {
+    var fname = window.prompt("Enter first name: ");
+    var lname = window.prompt("Enter last name: ");
+    var gmail = window.prompt("Enter mail: ");
+}
+function send()
+{
+    var to = window.prompt("Enter recipient");
+    var amt = window.prompt("Enter amount");
+    var method = window.prompt("Enter payment method");
+
+}
+function history(){
+    alert("No recent transactions");
+}
